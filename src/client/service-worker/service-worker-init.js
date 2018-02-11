627ca140be10d4e4aa5064ec9c078c4edd531ea4
Ding Guo <ding.guo@redballoon.com.au>
2018-02-11 13:51:24 +1100

    fix sw.js

---
+++ src/client/service-worker/service-worker-init.js
@@ -0,0 +1,14 @@
+
+export const register = () => {
+  if (window && 'serviceWorker' in window.navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/sw.js')
+        .then(registration => {
+          console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        })
+        .catch(err => {
+          console.log('ServiceWorker registration failed: ', err);
+        })
+    });
+  }
+};
